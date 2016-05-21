6f7e3606a9b63053df9e54d539d1892ea7561c6a
Quinntyne Brown <quinntynebrown@gmail.com>
2016-05-20 22:29:34 -0400

    fix template

--- Leora.Commands.Angular1/GenerateServiceCommand.cs
+++ Leora.Commands.Angular1/GenerateServiceCommand.cs
@@ -2,21 +2,24 @@
 using Leora.Commands.Angular1.Options;
 using Leora.Models;
 using Leora.Services.Contracts;
-using System;
 using static System.IO.File;
 
 namespace Leora.Commands.Angular1
 {
     public class GenerateServiceCommand : BaseCommand<GenerateServiceOptions>, IGenerateServiceCommand
     {
-        public GenerateServiceCommand(ITemplateManager templateManager, ITemplateProcessor templateProcessor)
-            : base(templateManager, templateProcessor)
-        { }
+        private readonly INamingConventionConverter _namingConventionConverter;
+
+        public GenerateServiceCommand(ITemplateManager templateManager, ITemplateProcessor templateProcessor, INamingConventionConverter namingConventionConverter)
+            : base(templateManager, templateProcessor) {
+            _namingConventionConverter = namingConventionConverter;
+        }
 
         public override int Run(GenerateServiceOptions options)
         {
             int exitCode = 1;
-            WriteAllLines($"{options.Directory}/{options.Name}.service.ts", _templateProcessor.ProcessTemplate(_templateManager.Get(Leora.Models.FileType.TypeScript, "Angular1Service"), options.Name));
+            WriteAllLines($"{options.Directory}\\{ _namingConventionConverter.Convert(NamingConvention.SnakeCase,options.Name)}.service.ts", 
+                _templateProcessor.ProcessTemplate(_templateManager.Get(Leora.Models.FileType.TypeScript, "Angular1Service"), options.Name));
             return exitCode;
         }
     }
