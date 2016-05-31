2f71c11f1eaface6d127b0c36d2f553941cb6c7e
Quinntyne Brown <quinntynebrown@gmail.com>
2016-05-30 22:13:19 -0400

    namespace

--- Leora.Services.Tests/NamespaceManagerTests.cs
+++ Leora.Services.Tests/NamespaceManagerTests.cs
@@ -6,9 +6,19 @@ namespace Leora.Services.Tests
     [TestClass]
     public class NamespaceManagerTests
     {
+        NamespaceManager _namespaceManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _namespaceManager = new NamespaceManager();
+        }
+
         [TestMethod]
         public void TestGetNamespaceMethod()
         {
+            var ns = _namespaceManager.GetNamespace(@"C:\projects\e-commerce\Server\Attributes\ChloeHandleErrorAttribute.cs");
+            Assert.AreEqual(ns, "Chloe.Server.Attributes");
         }
     }
 }
--- Leora.Services/NamespaceManager.cs
+++ Leora.Services/NamespaceManager.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using static System.String;
 using static System.Xml.Linq.XDocument;
 using System.Xml.Linq;
+using System.Collections.Generic;
 
 namespace Leora.Services
 {
@@ -14,22 +15,34 @@ namespace Leora.Services
 
         public string GetNamespace(string path)
         {
+            var namespaces = new List<string>();
             var projectPath = GetProjectPath(path);
             var csproj = Load(projectPath);
-            var projectGroups = csproj.Descendants(msbuild + "ProjectGroup");
-            return "";
+            var projectGroups = csproj.Descendants(msbuild + "PropertyGroup");
+            var rootNamespaceNode = projectGroups.Descendants(msbuild + "RootNamespace").First();
+            namespaces.Add((string)rootNamespaceNode.Value);
+            foreach(var ns in GetSubNamespaces(path, projectPath))
+                namespaces.Add(ns);
+            return Join(".", namespaces);            
         }
 
         public string GetProjectPath(string path, int depth = 0)
         {
             var directories = GetDirectoryName(path).Split(DirectorySeparatorChar);
             var newDirectories = directories.Take(directories.Length - depth).ToArray();
-            var final = Join(DirectorySeparatorChar.ToString(), newDirectories);
-            var projectFiles = GetFiles(final, "*.csproj");
+            var computedPath = Join(DirectorySeparatorChar.ToString(), newDirectories);
+            var projectFiles = GetFiles(computedPath, "*.csproj");
             if (projectFiles.FirstOrDefault() != null)
                 return projectFiles.First();
             depth = depth + 1;
             return GetProjectPath(path, depth);
+        }
+
+        public List<string> GetSubNamespaces(string path, string projectPath)
+        {
+            var pathDirectories = GetDirectoryName(path).Split(DirectorySeparatorChar);
+            var skip = GetDirectoryName(projectPath).Split(DirectorySeparatorChar).Count();
+            return pathDirectories.Skip(skip).Take(pathDirectories.Length - skip).ToList();
         }
     }
 }
