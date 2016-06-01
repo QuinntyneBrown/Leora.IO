a5c41d2e1ea886bc2098bc38d3651b7b585ef0c5
Quinntyne Brown <quinntynebrown@gmail.com>
2016-06-01 19:34:40 -0400

    add declaration file

---
+++ Leora.Commands.Angular1/Contracts/IGenerateDeclarationFileCommand.cs
@@ -0,0 +1,9 @@
+﻿using Leora.Commands.Contracts;
+
+namespace Leora.Commands.Angular1.Contracts
+{
+    public interface IGenerateDeclarationFileCommand : ICommand
+    {
+        int Run(string name, string directory);
+    }
+}
---
+++ Leora.Commands.Angular1/GenerateDeclarationFileCommand.cs
@@ -0,0 +1,28 @@
+﻿using Leora.Commands.Angular1.Contracts;
+using Leora.Commands.Angular1.Options;
+using Leora.Models;
+using Leora.Services.Contracts;
+using System;
+
+namespace Leora.Commands.Angular1
+{
+    public class GenerateDeclarationFileCommand : BaseCommand<GenerateDeclarationFileCommandOptions>, IGenerateDeclarationFileCommand
+    {
+        public GenerateDeclarationFileCommand(ITemplateManager templateManager, ITemplateProcessor templateProcessor, INamingConventionConverter namingConventionConverter, IProjectManager projectManager, IFileWriter fileWriter)
+            :base(templateManager,templateProcessor, namingConventionConverter,projectManager, fileWriter) { }
+
+        public override int Run(GenerateDeclarationFileCommandOptions options) => Run(options.Name, options.Directory);
+
+        public int Run(string name, string directory)
+        {
+            int exitCode = 1;
+            var snakeCaseName = _namingConventionConverter.Convert(NamingConvention.SnakeCase, name);
+            var typeScriptFileName = $"{snakeCaseName}.d.ts";
+            var baseFilePath = $"{directory}//{snakeCaseName}";
+            _fileWriter.WriteAllLines($"{baseFilePath}.d.ts", _templateProcessor.ProcessTemplate(_templateManager.Get(FileType.TypeScript, "Angular1DeclarationFile", BluePrintType.Angular1), name));
+            _projectManager.Process(directory, typeScriptFileName, FileType.TypeScript);
+
+            return exitCode;
+        }
+    }
+}
