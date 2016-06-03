3d1877dd6c6017c02e8616afa266a49fe4afdd57
Quinntyne Brown <quinntynebrown@gmail.com>
2016-06-02 20:05:59 -0400

    vs

---
+++ Leora.Commands.VisualStudio/GenerateProjectCommand.cs
@@ -0,0 +1,24 @@
+using System;
+using Leora.Commands.VisualStudio.Contracts;
+using Leora.Commands.VisualStudio.Options;
+using Leora.Models;
+using Leora.Services.Contracts;
+using static System.IO.File;
+
+namespace Leora.Commands.VisualStudio
+{
+    public class GenerateProjectCommand : BaseCommand<GenerateProjectOptions>, IGenerateProjectCommand
+    {
+        public GenerateProjectCommand(ITemplateManager templateManager, IDotNetTemplateProcessor templateProcessor, INamingConventionConverter namingConventionConverter, IProjectManager projectManager, IFileWriter fileWriter, INamespaceManager namespaceManager)
+            :base(templateManager,templateProcessor,namingConventionConverter,projectManager,fileWriter,namespaceManager) {
+        }
+        public override int Run(GenerateProjectOptions options) => Run(options.Name, options.Directory);        
+
+        public int Run(string name, string directory)
+        {
+            int exitCode = 1;
+            
+            return exitCode;
+        }
+    }
+}
