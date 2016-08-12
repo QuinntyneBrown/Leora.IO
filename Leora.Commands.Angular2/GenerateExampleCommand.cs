13a5c3f7a336bc7e5687dcf4c16799328ac00050
Quinntyne Brown <quinntynebrown@gmai.com>
2016-08-12 13:38:31 -0400

    update

--- Leora.Commands.Angular2/GenerateExampleCommand.cs
+++ Leora.Commands.Angular2/GenerateExampleCommand.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using Leora.Commands.Angular2.Contracts;
 using Leora.Services.Contracts;
+using Leora.Models;
 
 namespace Leora.Commands.Angular2
 {
     public class GenerateExampleCommand : IGenerateExampleCommand
     {
         private readonly ITemplateManager _templateManager;
+        private readonly IGenerateBootstrapCommand _generateBootstrapCommand;
+        private readonly IFileWriter _fileWriter;
+        private readonly ITemplateProcessor _templateProcessor;
 
-        public GenerateExampleCommand(ITemplateManager templateManager)
+        public GenerateExampleCommand(
+            ITemplateManager templateManager,
+            IGenerateBootstrapCommand generateBootstrapCommand)
         {
             _templateManager = templateManager;
+            _generateBootstrapCommand = generateBootstrapCommand;
         }
 
         public int Run(string[] args)
-        {
+        {            
             throw new NotImplementedException();
+        }
+
+        public int Run(string name)
+        {
+            var exitCode = 1;
+            _fileWriter.WriteAllLines("example.html", _templateProcessor.ProcessTemplate(_templateManager.Get(FileType.Html, "Angular2Example", BluePrintType.Angular2), name));
+            _fileWriter.WriteAllLines("bootstrap.ts", _templateProcessor.ProcessTemplate(_templateManager.Get(FileType.TypeScript, "Angular2Bootstrap", BluePrintType.Angular2), name));
+            return exitCode;
         }
     }
 }
