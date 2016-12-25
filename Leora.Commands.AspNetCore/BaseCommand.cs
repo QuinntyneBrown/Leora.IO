f73241aa2802de225481346e90149c880a78cdac
Quinntyne Brown <quinntynebrown@gmail.com>
2016-12-25 10:49:57 -0500

    AspNetCore

---
+++ Leora.Commands.AspNetCore/BaseCommand.cs
@@ -0,0 +1,37 @@
+﻿using static CommandLine.Parser;
+using Leora.Services.Contracts;
+using System;
+
+namespace Leora.Commands.AspNetCore
+{
+    public abstract class BaseCommand<TOptions> where TOptions : Leora.Commands.AspNetCore.Options.BaseOptions, new()
+    {
+        protected readonly ITemplateManager _templateManager;
+        protected readonly IDotNetTemplateProcessor _templateProcessor;
+        protected readonly INamingConventionConverter _namingConventionConverter;
+        protected readonly IProjectManager _projectManager;
+        protected readonly INamespaceManager _namespaceManager;
+        protected readonly IFileWriter _fileWriter;
+
+        public BaseCommand(IFileWriter fileWriter, ITemplateManager templateManager, IDotNetTemplateProcessor templateProcessor, INamingConventionConverter namingConventionConverter, INamespaceManager namespaceManager, IProjectManager projectManager)
+        {
+            _fileWriter = fileWriter;
+            _templateProcessor = templateProcessor;
+            _templateManager = templateManager;
+            _namingConventionConverter = namingConventionConverter;
+            _projectManager = projectManager;
+            _namespaceManager = namespaceManager;
+        }
+
+        public virtual int Run(string[] args)
+        {
+            var options = new TOptions();
+            Default.ParseArguments(args, options);
+            options.NameSpace = _namespaceManager.GetNamespace(options.Directory).Namespace;
+            options.RootNamespace = _namespaceManager.GetNamespace(options.Directory).RootNamespace;
+            return Run(options);
+        }
+
+        public abstract int Run(TOptions options);
+    }
+}
